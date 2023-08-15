e398f39b2ec7207674253837fbbe4dca4b2ea15c
Himanshu Kumar <himanshukr033@gmail.com>
2023-08-16 00:41:38 +0530

    bug fixing

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, CardContent, CardMedia, Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 import styles from './Channel.module.css';
 
